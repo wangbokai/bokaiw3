3ef49add66d2aa873e37d6c8718ce47ef242cf1b
wangbokai <wang_bokai@hotmail.com>
2015-12-14 19:13:55 -0500

    2nd push
    2nd push

---
+++ R/adjCIs1.R
@@ -0,0 +1,10 @@
+## a function to get 95% CIs for a or b
+## fit is the model fitted before...
+## set level = 0.95 to get a 95% CI so can change level = 0.99 if wanna a 99% CI
+
+adjCIs1 = 4*confint(fit, level = 0.95)
+
+## slfit1 = lm(exp.C_value.sl1 ~ 0 + exp.A_value) is the simple linear regression model
+## to solve a by leting b=1, and get a 95 percent CI for a
+## show the 95 percent CI for a in this case
+## 4*confint(slfit1, level = 0.95)
---
+++ R/adjCIs2.R
@@ -0,0 +1,10 @@
+## a function to get 95% CIs for a and b
+## fit is the model fitted before...
+## set level = 0.95 to get a 95% CI so can change level = 0.99 if wanna a 99% CI
+
+adjCIs2 = 4*confint(fit, level = 0.95)[,2]/sum(confint(fit, level = 0.95)[,2])
+
+## mlfit1 = lm(exp.C_value ~ 0 + exp.A_value + exp.B_value) is the multiple linear regression model
+## to solve a and b, and get a 95 percent CIs for a and b
+## construct the 95 percent CIs for the case when a=3? and b=1?
+## 4*confint(mlfit1, level = 0.95)[,2]/sum(confint(mlfit1, level = 0.95)[,2])
