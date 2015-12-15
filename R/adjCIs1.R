98926507cbdfa881d159874639828ebe392b5437
wangbokai <wang_bokai@hotmail.com>
2015-12-14 19:50:12 -0500

    3nd push
    3nd push

--- R/adjCIs1.R
+++ R/adjCIs1.R
@@ -2,7 +2,9 @@
 ## fit is the model fitted before...
 ## set level = 0.95 to get a 95% CI so can change level = 0.99 if wanna a 99% CI
 
-adjCIs1 = 4*confint(fit, level = 0.95)
+adjCIs1 = function(fit) {
+  return(4*confint(fit, level = 0.95))
+  }
 
 ## slfit1 = lm(exp.C_value.sl1 ~ 0 + exp.A_value) is the simple linear regression model
 ## to solve a by leting b=1, and get a 95 percent CI for a
